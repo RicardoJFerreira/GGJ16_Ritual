0675aaee33faedd4bfe1302d3c8f809ba32558c5
Guilherme <guilherme.m.d.bento@gmail.com>
2016-01-30 16:13:27 +0000

    Added pause menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool isPaused = false; // On pause or not
+
+    void Start()
+    {
+     
+    }
+
+
+    void Update()
+    {
+        // No longer paused
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = !isPaused;
+        }
+        TogglePauseMenu();
+    }
+
+    public void TogglePauseMenu()
+    {
+        // not the optimal way but for the sake of readability
+        if (!isPaused)
+        {
+            gameObject.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            gameObject.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+
+        Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
+    }
+
+
+// Methods for onClick()
+
+public void StartScene()
+    {
+        Application.LoadLevel("level_1");
+    }
+
+    public void ContinueScene()
+    {
+        isPaused = false;
+       // TogglePauseMenu();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
