1456157f1bbb0da8a20ab07ebf944b76dfccb06d
Guilherme <guilherme.m.d.bento@gmail.com>
2016-01-30 13:03:29 +0000

    Menu

--- Assets/Scripts/Menu/MenuButtons.cs
+++ Assets/Scripts/Menu/MenuButtons.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class MenuButtons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void StartScene()
+    {
+        Application.LoadLevel("level_1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
